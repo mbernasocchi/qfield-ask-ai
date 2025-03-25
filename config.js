652b2db12d8ec1a7a3250e377018a5b55222b4dd
Marco Bernasocchi <marco@opengis.ch>
2025-03-26 00:13:45 +0100

    add headers to config

--- config.js
+++ config.js
@@ -8,15 +8,28 @@ const apis = [
       "claude-3-5-sonnet-latest",
       "claude-3-5-haiku-latest",
     ],
+    headers: {
+      "x-api-key": "%%API_KEY%%",
+      "anthropic-version": "2023-06-01",
+      "content-type": "application/json",
+    },
   },
   {
     name: "OpenAI",
     url: "https://api.openai.com/v1/chat/completions",
     models: ["gpt-4o", "gpt-4.5", "gpt-3.5-turbo"],
+    headers: {
+      "Authorization": "Bearer %%API_KEY%%",
+      "content-type": "application/json",
+    },
   },
   {
     name: "Infomaniak",
     url: "https://api.infomaniak.com/1/ai/102849/openai/chat/completions",
     models: ["llama3", "whisper", "reasoning"],
+    headers: {
+      "Authorization": "Bearer %%API_KEY%%",
+      "content-type": "application/json",
+    },
   },
 ];
