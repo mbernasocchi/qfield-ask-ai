4350c406012955c4a044db4b9651d5d852112f1a
Mathieu Pellerin <nirvn.asia@gmail.com>
2025-07-12 11:26:55 +0700

    Be friendlier to newcomers by providing them with a URL to register an API key and by using API provider (i.e. anthropic, openai) instead of raw URL

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const apis = [
   {
     // Anthropic API is the default
     name: "Anthropic",
+    key_url: "https://console.anthropic.com/login?selectAccount=true&returnTo=%2Fsettings%2Fadmin-keys%3F",
     url: "https://api.anthropic.com/v1/messages",
     models: [
       "claude-3-7-sonnet-latest",
@@ -16,6 +17,7 @@ const apis = [
   },
   {
     name: "OpenAI",
+    key_url: "https://platform.openai.com/api-keys",
     url: "https://api.openai.com/v1/chat/completions",
     models: ["gpt-4o", "gpt-4.5", "gpt-3.5-turbo"],
     headers: {
